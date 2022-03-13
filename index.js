72541f1c289f8d5f8c3941cf875de46f291ef18e
gomez-fernando <ferdgomez@hotmail.es>
2022-03-13 23:41:38 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,231 @@
+let name;
+let scale = "";
+let choice;
+let toDrop;
+var flights2 = [];
+var flights = [
+
+  // { id: 00, to: 'Bilbao', from: 'Barcelona', cost: 1600, scale: false },
+
+  { id: 01, to: 'New York', from: 'Barcelona', cost: 700, scale: false },
+
+  { id: 02, to: 'Los Angeles', from: 'Madrid', cost: 1100, scale: true },
+
+  { id: 03, to: 'Paris', from: 'Barcelona', cost: 210, scale: false },
+
+  { id: 04, to: 'Roma', from: 'Barcelona', cost: 150, scale: false },
+
+  { id: 05, to: 'London', from: 'Madrid', cost: 200, scale: false },
+
+  { id: 06, to: 'Madrid', from: 'Barcelona', cost: 90, scale: false },
+
+  { id: 07, to: 'Tokyo', from: 'Madrid', cost: 1500, scale: true },
+
+  { id: 08, to: 'Shangai', from: 'Barcelona', cost: 800, scale: true },
+
+  { id: 09, to: 'Sydney', from: 'Barcelona', cost: 150, scale: true },
+
+  { id: 10, to: 'Tel-Aviv', from: 'Madrid', cost: 150, scale: false }
+];
+
+const average = (array) => {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++){ 
+    sum += array[i].cost;
+  }
+  return ((sum / array.length).toFixed(2));
+}
+
+const showFlights = () => {
+  console.log("Admin dashboard:\nLista actualizada de vuelos:\n")
+
+  for (let i = 0; i < flights.length; i++) {
+    (flights[i].scale)
+      ? scale = "Sí"
+      : scale = "No";
+    console.log(`ID: ${flights[i].id} | Origen: ${flights[i].from} | Destino: ${flights[i].to} | Coste: ${flights[i].cost} | Escala: ${scale}`);
+  }
+}
+
+const admin = () => {
+  do {
+    choice = prompt(`Menú:
+Elige una opción indicando el número:
+1: Crear vuelo
+2: Eliminar vuelo
+3: Cerrar programa`);
+
+    switch (choice) {
+      case '1':
+        create();
+        break;
+
+      case '2':
+        drop();
+        break;
+
+      case '3':
+        close();
+        break;
+
+      default :
+        choice = null;
+        break;
+    }
+  } while (choice !== '1' && choice !== '2' && choice !== '3');
+}
+
+const create = () => {
+  let newFlight = {id : undefined, to: "", from: "", cost: undefined, scale: undefined};
+  
+  do {
+    newFlight['to'] = prompt(`Ingresa la ciudad de destino del vuelo`);
+  } while (newFlight['to'] === null || newFlight['to'] === "");
+
+  do {
+    newFlight['from'] = prompt(`Ingresa la ciudad de origen del vuelo`);
+  } while (newFlight['from'] === null || newFlight['from'] === "");
+
+  do {
+    newFlight['cost'] = prompt(`Ingresa el precio del vuelo`);
+    if(isNaN(newFlight['cost'])){
+      newFlight['cost'] = null;
+    } else{
+      newFlight['cost'] = parseFloat(newFlight['cost']);
+    }
+  } while (newFlight['cost'] === null || newFlight['cost'] === "");
+
+  do {
+    newFlight['scale'] = prompt(`Indica si el vuelo tiene escalas: 's/n'`);
+
+    switch (newFlight['scale']) {
+      case 's':
+        newFlight['scale'] = true;
+        break;
+
+      case 'n':
+        newFlight['scale'] = false;
+        break;
+
+      default :
+        newFlight['scale'] = null;
+        break;
+    }
+  } while (newFlight['scale'] === null || newFlight['scale'] === "");
+
+  let ids = flights.map( x => {
+      return x.id;
+  })
+
+  let maxId = Math.max(...ids);
+
+  newFlight['id'] = maxId + 1;
+  flights.push(newFlight);
+
+  showFlights();
+
+  alert("Vuelo creado correctamente, puedes ver la lista actualizada en la consola.");
+
+  admin();
+}
+
+// TODO limitar a 14 los vuelos en el array
+
+const drop = () => {
+  do {
+    toDrop = prompt(`Indica el id del vuelo que quieres borrar:`);
+    toDrop = parseInt(toDrop);
+    
+    let ids = flights.map( x => {
+      return x.id;
+    })
+
+    let idToDrop = ids.indexOf(toDrop);
+
+    if(idToDrop === -1) {
+      toDrop = null;
+      alert("El id introducido no existe.");
+    } else{
+      flights2 = flights.splice(idToDrop, 1);
+
+      showFlights();
+
+      alert("El vuelo ha sido eliminado correctamente, puedes ver la lista actualizada en la consola.");
+
+      admin();
+    }
+    
+  } while (toDrop === null || toDrop === "");
+}
+
+const close = () => {
+  alert("Has cerrado sesión.\nBye !");
+}
+
+const user = () => {
+  alert("hola user")
+}
+
+const airline = () => {
+  do {
+    name = prompt(`Bienvenido/a a SkyLab Airlines !!
+Dinos tu nombre`);
+  
+  } while (name === null || name === "");
+  
+
+  console.info(`Hola ${name},
+Esta es la lista de vuelos para hoy: \n\n`);
+  for (let i = 0; i < flights.length; i++) {
+    (flights[i].scale)
+      ? scale = "Sí"
+      : scale = "No";
+    console.log(`ID: ${flights[i].id} | Origen: ${flights[i].from} | Destino: ${flights[i].to} | Coste: ${flights[i].cost} | Escala: ${scale}`);
+  }
+
+  console.log('\n*****************');
+  console.info(`El coste medio de los vuelos es:
+$ ${average(flights)}`);
+  console.log('\n*****************');
+
+  let totalWithScales = 0;
+  for (let i = 0; i < flights.length; i++) {
+    if (flights[i].scale) totalWithScales++;
+  };
+  
+  console.info(`Los vuelos que realizan escalas son: ${totalWithScales}`);
+
+  console.log('\n*****************');
+  console.info('Los últimos 5 vuelos del día son: \n');
+
+  for (let i = flights.length - 1; i > (flights.length - 6); i--) {
+console.log(`ID: ${flights[i].id} Destino: ${flights[i].to} | Origen: ${flights[i].from}`);
+  }
+  
+  let role = "";
+  do {
+    role = prompt(`LOGIN
+Indica si eres ADMIN o USER`);
+
+    if(role !== null) role = role.toLocaleLowerCase();
+
+    switch (role) {
+      case 'admin':
+        admin();
+        break;
+
+      case 'user':
+        user();
+        break;
+
+      default :
+        role = null;
+        break;
+    }
+
+  } while (role === null || role === "");
+}
+
+
+
+airline();
